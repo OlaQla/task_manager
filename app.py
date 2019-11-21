07df8d52791b229c7674d0e47c2fb232965770f0
Aleksandra Kula <aleksandra.kula@outlook.com>
2019-11-21 09:04:45 +0000

    added route to add_task section

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def get_tasks():
 
 @app.route('/add_task')
 def add_task():
-    return render_template("addtask.html")
+    return render_template('addtask.html')
 
 if __name__ == '__main__':
     app.run(host="localhost", port="5000", debug=True)
